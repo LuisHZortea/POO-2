b2f600c2d43c525001dc0fbc94741d982646c7c1
Luis Henrique Gomes Zortea <102925017+LuisHZortea@users.noreply.github.com>
2023-08-10 08:52:01 -0300

    Create Carro.java

---
+++ Carro.java
@@ -0,0 +1,9 @@
+public class Veiculo {
+    private String marca;
+    private String cor;
+    private String placa;
+    private int ano;
+    
+    private Modelo modelo; 
+    private Seguro seguro;
+} 
